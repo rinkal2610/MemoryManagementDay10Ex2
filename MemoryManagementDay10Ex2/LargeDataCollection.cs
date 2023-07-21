dc9239d59799280cca2db5e471d2181bf5b5dc12
rinkal2610 <rinkalpatel2233@gmail.com>
2023-07-21 11:47:18 +0530

    Memory Management-exercise

---
+++ MemoryManagementDay10Ex2/LargeDataCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagementDay10Ex2
+{
+    public class LargeDataCollection : IDisposable
+    {
+        List<string> list=new List<string>() { "sam", "rahul", "raj", "aditi" };
+        static int totalItems;
+        bool disposedValue;
+        public LargeDataCollection() 
+        {
+            totalItems = 0;
+            this.list = list;
+        }
+        public void Add(string item)
+        {
+            list.Add(item);
+        }
+        public void Remove(string item)
+        {
+            list.Remove(item);
+        }
+        public void Print()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposing) totalItems= 0;
+            Console.WriteLine("the items are diposed and");
+            disposedValue = true;
+
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ MemoryManagementDay10Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagementDay10Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            LargeDataCollection obj=new LargeDataCollection();
+            Console.WriteLine("enetr an item to add");
+            string item1=Console.ReadLine();
+            obj.Add(item1);
+            Console.WriteLine("enetr an item to remove");
+            string item2 = Console.ReadLine();
+            obj.Remove(item2);
+            Console.WriteLine("list of items");
+            obj.Print();
+            obj.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
